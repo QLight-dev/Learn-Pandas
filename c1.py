73aadaebed2dda8f02e9bb5fb0080ea82d113cbd
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-09 19:05:16 +0400

    In Progress: Chapter 1

--- c1.py
+++ c1.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+# Creating a Series
+list = [1, 6, 9]
+Series = pd.Series(list)
+print(f"Pandas Series: \n{Series}")
+
+# Create Lables
+list = [3, 8, 7]
+Series = pd.Series(list, index = ['a', 'b', 'c'])
